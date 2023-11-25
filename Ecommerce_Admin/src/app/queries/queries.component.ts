2c63555092e6a744b1e94363e2ff6592d18977eb
klokesh1999 <klokesh1999@gmail.com>
2023-11-25 17:21:16 +0000

    Manage Queries UI Completed

--- Ecommerce_Admin/src/app/queries/queries.component.ts
+++ Ecommerce_Admin/src/app/queries/queries.component.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { FormBuilder, Validators } from '@angular/forms';
+
+export interface Query {
+  name: string;
+  email: string;
+  mobileNumber: number;
+  query: string;
+  status: string;
+  resolution: string;
+}
 
 @Component({
   selector: 'app-queries',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './queries.component.html',
   styleUrl: './queries.component.css'
 })
 export class QueriesComponent {
 
+
+  data: Query[] = [
+    { name: 'Lokesh', email: 'klokesh1999@gmail.com', mobileNumber: 8978527113, query: "Hi Admin, Please Check this issue", status: 'Pending', resolution: '' },
+    { name: 'Lokesh', email: 'klokesh1999@gmail.com', mobileNumber: 8978527113, query: "Hi Admin, Please Check this issue", status: 'Resolved', resolution: 'We will get back to you shortly' },
+    { name: 'Lokesh', email: 'klokesh1999@gmail.com', mobileNumber: 8978527113, query: "Hi Admin, Please Check this issue", status: 'Pending', resolution: '' },
+    { name: 'Lokesh', email: 'klokesh1999@gmail.com', mobileNumber: 8978527113, query: "Hi Admin, Please Check this issue", status: 'Pending', resolution: '' },
+    { name: 'Lokesh', email: 'klokesh1999@gmail.com', mobileNumber: 8978527113, query: "Hi Admin, Please Check this issue", status: 'Pending', resolution: '' },
+    { name: 'Lokesh', email: 'klokesh1999@gmail.com', mobileNumber: 8978527113, query: "Hi Admin, Please Check this issue", status: 'Resolved', resolution: 'We will get back to you shortly' },
+    { name: 'Lokesh', email: 'klokesh1999@gmail.com', mobileNumber: 8978527113, query: "Hi Admin, Please Check this issue", status: 'Pending', resolution: '' },
+    { name: 'Lokesh', email: 'klokesh1999@gmail.com', mobileNumber: 8978527113, query: "Hi Admin, Please Check this issue", status: 'Pending', resolution: '' },
+  ]
+
+  displayedColumns: string[] = Object.keys(this.data[0]);
+
+  constructor(private fb: FormBuilder) { }
+
+  sendResolution(value: string) {
+    console.log(value)
+  }
 }

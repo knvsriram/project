55852156c310303abc750dd06636dcb737eeeef3
klokesh1999 <klokesh1999@gmail.com>
2023-11-30 17:40:30 +0000

    Users component done

--- Ecommerce_Admin/src/app/users/users.component.ts
+++ Ecommerce_Admin/src/app/users/users.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+export interface User {
+  name: string;
+  email: string;
+  mobileNumber: number;
+  isUserConfirmed: boolean
+}
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatSlideToggleModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
 
+  data: User[] = [
+    { name: 'Lokesh', email: 'klokesh1999@gmail.com', mobileNumber: 8978527113, isUserConfirmed: true },
+    { name: 'Lokesh', email: 'klokesh1999@gmail.com', mobileNumber: 8978527113, isUserConfirmed: true },
+    { name: 'Lokesh', email: 'klokesh1999@gmail.com', mobileNumber: 8978527113, isUserConfirmed: true },
+    { name: 'Lokesh', email: 'klokesh1999@gmail.com', mobileNumber: 8978527113, isUserConfirmed: true },
+    { name: 'Lokesh', email: 'klokesh1999@gmail.com', mobileNumber: 8978527113, isUserConfirmed: true },
+  ]
+  displayedColumns: string[] = Object.keys(this.data[0]);
+
+  change() {
+    console.log("Changed")
+  }
 }
